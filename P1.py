ddb47f19a5a6977508e68455447ba0fcdf7d6d17
Adam White <adam.nathaniel.white@gmail.com>
2015-09-25 22:38:14 -0400

    Fix array pop after solution had been found. Everything is functional.

--- P1.py
+++ P1.py
@@ -12,33 +12,63 @@ from roomba_sim import *
 from roomba_concurrent import *
 
 goalMoves = 135
-heuristic = None #which heuristic method to use
+#heuristic = None #which heuristic method to use
 
-def dijsktrasHeuristic(dirt):
+class concurrencyManager(object):
+    def __init__(self):
+        self.completed = False # Whether we have found the solution. Kill other threads if true.
+        self.optimalSolution = [] # Stores the optimal solution
+
+    def storeSolution(self, solution):
+        for move in solution:
+            self.optimalSolution.append(move)
+
+    def isCompleted(self):
+        return self.completed
+
+def simpleNumDirtyHeuristic(dirt, location):
+    #Just returns the amount of dirt left
+    if location in dirt :
+        return (len(dirt) * 1.8) - 1
+    return len(dirt) * 1.9
+
+def dijsktrasHeuristic(dirt, location):
+    #This is related to the travelling salesperson problem
     #TODO
     return 0
 
-def solver(node, walls):
+def solver(node, walls, heuristic, CM):
+    numExplored = 0
     (movements, location, dirt) = node
-    newWeight = 0 + dijsktrasHeuristic(dirt)
+    newWeight = 0 + heuristic(dirt, location)
     q = [] #heapq
     heapq.heappush(q, (newWeight, movements, location, dirt))
-    while (len(dirt) > 0) :
-        print("popping:")
+    while (len(dirt) > 0 and CM.isCompleted() == False) :
+        numExplored += 1
         (weight, movements, location, dirt) = heapq.heappop(q)
-        print("popped node: weight", weight, " movements ", movements, " location ", location, " dirt ", dirt)
-        if ( dirt == [] ) :
-            if (len(bestSolutionMovements) > goalMoves) :
-                print("found a solution which is ", len(movements), " long. It is longer than the specified max of ", goalMoves, " long. Continuing search..")
-            return movements
+        #print("popped node: weight", weight, " movements ", movements, " location ", location, " dirt ", dirt)
+        if ( len(dirt) <= 0 ) :
+            if (len(movements) < goalMoves) :
+                print("")
+                print("")
+                print("--------STATISTICS!!! -----------")
+                print("")
+                print("found a solution which is ", len(movements), " long.")
+                print("The total number of possibilities explored was ", numExplored)
+                print("The maximum number of frontier object stored in heapq at one time was: ", " (not implemented yet)")
+                print("")
+                CM.storeSolution(movements)
+                CM.completed = True
+                return movements
+            print("found a solution which is ", len(movements), " long. However, it is longer than the specified max of ", goalMoves, " long. Continuing search..")
 
         if location in dirt :
-            print("roomba is on top of dirt")
+            #print("roomba is on top of dirt")
             newDirt = copy.deepcopy(dirt) #slow #todo: this deepcopy can be eliminated, since we are returing immediately after this
             newDirt.remove(location)
             newMovements = copy.deepcopy(movements) #slow #todo: this deepcopy can be eliminated, since we are returing immediately after this
             newMovements.append('Suck')
-            newWeight = len(newMovements) + dijsktrasHeuristic(newDirt) #A* heuristic
+            newWeight = len(newMovements) + heuristic(newDirt, location) #A* heuristic
             heapq.heappush(q, (newWeight, newMovements, location, newDirt))
             continue
 
@@ -50,7 +80,7 @@ def solver(node, walls):
         if ( not (( newLocation in walls) )) : #or (newX > roomWidth))) :
             newMovements = copy.deepcopy(movements) #slow
             newMovements.append('East')
-            newWeight = weight + 1 #len(newMovements) + dijsktrasHeuristic #A* heuristic #TODO: does this even change if nothing is sucked?
+            newWeight = weight + 1 + heuristic(dirt, newLocation) #A* heuristic 
             heapq.heappush(q, (newWeight, newMovements, newLocation, dirt))
 
         #West
@@ -59,32 +89,34 @@ def solver(node, walls):
         if ( not ((newLocation in walls) )) : # or (newX <= 0))) :
             newMovements = copy.deepcopy(movements) #slow
             newMovements.append('West')
-            newWeight = weight + 1 #len(newMovements) + dijsktrasHeuristic #A* heuristic #TODO: does this even change if nothing is sucked?
+            newWeight = weight + 1 + heuristic(dirt, newLocation) #A* heuristic 
             heapq.heappush(q, (newWeight, newMovements, newLocation, dirt))
 
         #North
-        newY = oldY - 1
+        newY = oldY + 1
         newLocation = (oldX, newY)
         if ( not ((newLocation in walls) )) : # or (newY <= 0))) :
             newMovements = copy.deepcopy(movements) #slow
             newMovements.append('North')
-            newWeight = weight + 1 #len(newMovements) + dijsktrasHeuristic #A* heuristic #TODO: does this even change if nothing is sucked?
+            newWeight = weight + 1 + heuristic(dirt, newLocation) #A* heuristic 
             heapq.heappush(q, (newWeight, newMovements, newLocation, dirt))
 
         #South
-        newY = oldY + 1
+        newY = oldY - 1
         newLocation = (oldX, newY)
         if ( not ((newLocation in walls) )) : # or (newY > roomHeight))) :
             newMovements = copy.deepcopy(movements) #slow
             newMovements.append('South')
-            newWeight = weight + 1 #len(newMovements) + dijsktrasHeuristic #A* heuristic #TODO: does this even change if nothing is sucked?
+            newWeight = weight + 1 + heuristic(dirt, newLocation) #A* heuristic 
             heapq.heappush(q, (newWeight, newMovements, newLocation, dirt))
             
-        
-        
+    print("Something went wrong and no solution was found")
+    assert(0)
+    
 
-class aStarRobot(DiscreteRobot):
-
+        
+class aStarRobot(DiscreteRobot, concurrencyManager):
+    
     def initialize(self, chromosome):
         self.state = None
         startCoords = self.getRobotPosition()
@@ -93,19 +125,34 @@ class aStarRobot(DiscreteRobot):
         startLY = int(startY)
         location = (startLX, startLY)
         firstNode = ([], location, self.getDirty())
-        
-        solutionMovements = solver(firstNode, self.getWalls())
+
+        self.CM = concurrencyManager()
+
+        # Select the heuristic we wish to use
+        heuristic = simpleNumDirtyHeuristic
+        #heuristic = dijsktrasHeuristic
+
+        #start multiple threads
+        #movesQueue = Queue() #multithreaded queue. TODO This needs to be the heapq
+        #solverProcess = Process(target=solver, args=(queue))
+        #solverProcess.start()
+
+        solution = solver(firstNode, self.getWalls(), heuristic, self.CM)
+        #print("solution here = ", solution)
+        #self.CM.storeSolution(solution)
   
     def runRobot(self):
-        print("runRobot")
-        (bstate, dirt) = self.percepts
+        #print("runRobot")
 
-        #TODO
-
-        #if RobotBase.getDirty() :
-        #self.action = 'Suck'
-        #else :
-        self.action = random.choice(['North','South','East','West'])
+##        if(len(self.CM.optimalSolution) <= 0):
+##            print("The movements array is empty. It thinks it is done.")
+##            dirt = self.getDirty()
+##            print("Finished with dirt", dirt)
+##            assert(0)
+        
+        nextMovement = self.CM.optimalSolution.pop(0)
+        self.action = nextMovement
+        return
       
     
 
@@ -154,17 +201,14 @@ mediumWalls5Room.setWall((26,25), (7,25))
 mediumWalls5Room.setWall((7,5), (7,22))
 allRooms.append(mediumWalls5Room) # [7]
 
+easyRoom = RectangularRoom(6,6,0.5)
+easyRoom.setWall((2,2), (2,4))
+allRooms.append(easyRoom) # [8] for debugging only, not graded
+
 #############################################    
 def aStar():
-    #start multiple threads
-    #movesQueue = Queue() #multithreaded queue. TODO This needs to be the heapq
-    #solverProcess = Process(target=solver, args=(queue))
-    #solverProcess.start()
-
-    #solution = solver(self)
-    
     print(runSimulation(num_trials = 2,
-                    room = allRooms[0], #Change room number here
+                    room = allRooms[8], #Change room number here
                     robot_type = aStarRobot,
                     ui_enable = True,
                     ui_delay = 0.01))
